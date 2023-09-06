337d86b41911dcc53970740bb35c1744909f3132
sinbawh <132280776+sinbawh@users.noreply.github.com>
2023-09-06 18:10:26 +0330

    redirecting all the urls to maintence

---
+++ mysite/setting/dev.py
@@ -0,0 +1,37 @@
+from mysite.settings import *
+
+#=-----------------------------
+#dev
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-e!3)0_%dtx_3ygcf(e5x579ye71w+gf^v3fqak2r+5-+xcov0f'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+#sites framework
+SITE_ID = 2
+
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR /'media'
+
+STATICFILES_DIRS = [BASE_DIR /"statics"]
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
--- mysite/urls.py
+++ mysite/urls.py
@@ -22,6 +22,8 @@ from website.sitemaps import StaticViewSitemap
 from blog.sitemaps import BlogSitemap
 import debug_toolbar
 from django.contrib.auth import views as auth_views
+from django.urls import re_path
+from django.views.generic import TemplateView
 
 
 
@@ -64,3 +66,6 @@ urlpatterns = [
                 ] 
 urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.MAINTENANCE_MODE:
+   urlpatterns.insert(0, re_path(r'^', TemplateView.as_view(template_name='maintenance.html'), name='maintenance'))
